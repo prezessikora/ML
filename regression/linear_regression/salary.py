0b69865a59d314436792e99a89852c4b5971b086
Krzysztof Sikora <prezes.sikora@gmail.com>
2024-09-20 14:12:48 +0200

    Small linear regression refactor and pip3 packages

--- regression/linear_regression/salary.py
+++ regression/linear_regression/salary.py
@@ -4,11 +4,7 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 
 # Load the dataset
-dataset = pd.read_csv('./linear_regression/Salary_Data.csv')
-
-#X = dataset['YearsExperience'].values
-#y = dataset['Salary'].values
-
+dataset = pd.read_csv('./regression/linear_regression/Salary_Data.csv')
 
 X = dataset.iloc[:,:-1].values
 y = dataset.iloc[:,-1].values
@@ -23,26 +19,19 @@ X_train, X_test, y_train, y_test = train_test_split(X,y,test_size = 0.2, random_
 from sklearn.linear_model import LinearRegression
 regressor = LinearRegression()
 regressor.fit(X_train, y_train)
-y_pred = regressor.predict(X_test);
+y_pred = regressor.predict(X_test)
 
+# Plot train data and fitted model (line)
 import matplotlib.pyplot as plt
 
-# Plot train data and fitted model (line)
+def plotDataAndFit(Xv,yv):
+    plt.scatter(Xv,yv, color = 'red')
+    plt.plot(Xv,regressor.predict(Xv), color='blue')
 
-plt.scatter(X_train,y_train, color = 'red')
-plt.plot(X_train,regressor.predict(X_train), color='blue')
+    plt.xlabel('Experience')
+    plt.ylabel('Salary')
+    plt.title('Experience vs Salary')
+    plt.show()
 
-plt.xlabel('Experience')
-plt.ylabel('Salary')
-plt.title('Experience vs Salary')
-plt.show()
-
-# Plot train data and fitted model (line)
-
-plt.scatter(X_test,y_test, color = 'red')
-plt.plot(X_train,regressor.predict(X_train), color='blue')
-
-plt.xlabel('Experience')
-plt.ylabel('Salary')
-plt.title('Experience vs Salary')
-plt.show()
+plotDataAndFit(X_train,y_train)
+plotDataAndFit(X_test,y_test)
